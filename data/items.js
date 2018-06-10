be88cf19823942df50fa3e6dda39ffdfbc7a4f66
Colin Ewen <colin@draecas.com>
2018-06-10 01:14:40 -0400

    Try not caching items

--- data/items.js
+++ data/items.js
@@ -2,9 +2,7 @@ const rp = require('request-promise');
 const cache = require('../services/cache').cache;
 
 const itemDefs = function() {
-  return cache.wrap('itemDefs', () => {
-    return rp({ uri: 'https://destiny.plumbing/en/raw/DestinyInventoryItemDefinition.json', json: true });
-  }, { ttl: 3600 });
+  return rp({ uri: 'https://destiny.plumbing/en/raw/DestinyInventoryItemDefinition.json', json: true });\
 }
 
 module.exports = { itemDefs };
